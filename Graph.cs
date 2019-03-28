cc0ccb3054823ac698cbf4aaf1d5f7470746f913
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-03-27 18:05:11 -0700

    More diffy q's

--- Graph.cs
+++ Graph.cs
@@ -11,6 +11,8 @@ public delegate Vector3 GraphFunction(Vector3 position, int resolution, float ti
 
 public enum GraphFunctionName {
     SpiralDiffEq,
+    HourGlassDiffEq,
+    CircleDiffEq,
     SineFunction,
     DoubleSineFunction,
     SineTimes2Function,
@@ -28,27 +30,21 @@ public class Graph : MonoBehaviour {
     public GraphFunctionName activeFunction;
     Point[] points;
     static GraphFunction[] functions = {
-        SpiralDiffEq, SineFunction, DoubleSineFunction, SineTimes2Function, WildinSineFunction, TanFunction, TanSineFunction
+        SpiralDiffEq, HourGlassDiffEq, CircleDiffEq, SineFunction, DoubleSineFunction, SineTimes2Function, WildinSineFunction, TanFunction, TanSineFunction
     };
     
 
     void Start() {
         float time = 0f;
-        //points = new Point[resolution * resolution];
         points = new Point[resolution];
         float step = (2f / resolution) * 5f;
         Vector3 scale = Vector3.one * step;
         Vector3 position;
         position.y = 0f;
         position.z = 0f;
-        for (int i = 0, x = 0, z = 0; i < points.Length; ++i, ++x) {
-            if (x == resolution) {
-                x = 0;
-                z += 1;
-            }
+        for (int i = 0; i < points.Length; ++i) {
             Transform point = Instantiate(pointPrefab);
             position.x = ((i + 0.5f) * step) - 5f;
-            //position.z = ((z + 0.5f) * step) - 5f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -65,22 +61,22 @@ public class Graph : MonoBehaviour {
         float time = Time.time;
         GraphFunction f = functions[(int)activeFunction];
 
-        for (int i = 0, x = 0; i < points.Length; ++i, ++x) {
-            if (x == resolution) {
-                x = 0;
-            }
+        for (int i = 0; i < points.Length; ++i) {
             Transform point = points[i].point;
             Vector3 position = point.localPosition;
-            if (activeFunction == 0) {
-                point.localPosition = f(position, resolution, points[i].time, c1, c2, x);
+            if(activeFunction == 0) {
+                point.localPosition = f(position, resolution, points[i].time, c1, c2, i);
+                if(points[i].time > 10) {
+                    points[i].time = 0;
+                }
+            }
+            else if((int)activeFunction == 1 || (int)activeFunction == 2) {
+                point.localPosition = f(position, resolution, points[i].time, c1, c2, i);
             } else {
-                point.localPosition = f(position, resolution, time, c1, c2, x);
+                point.localPosition = f(position, resolution, time, c1, c2, i);
             }
             points[i].point = point;
             points[i].time += 0.02f;
-            if (points[i].time > 10) {
-                points[i].time = 0;
-            }
         }
     }
 
@@ -88,6 +84,20 @@ public class Graph : MonoBehaviour {
         float e = 1 * Mathf.Exp(-time);
         position.x = ((c1 * e) * ((1 * Mathf.Cos(Mathf.PI * time)) - Mathf.Sin(Mathf.PI * time))) + ((c2 * e) * (Mathf.Cos(Mathf.PI * time) + (1 * Mathf.Sin(Mathf.PI * time))));
         position.y = ((c1 * e) * (1 * Mathf.Cos(Mathf.PI * time))) + ((c2 * e) * (1 * Mathf.Sin(Mathf.PI * time)));
+
+        return position;
+    }
+
+    public static Vector3 HourGlassDiffEq(Vector3 position, int resolution, float time, float c1, float c2, int i) {
+        position.x = c1 * (Mathf.Cos(4 * time)) + c2 * (Mathf.Sin(4 * time));
+        position.y = c1 * (Mathf.Cos(time) + 3 * Mathf.Sin(time)) + c2 * (Mathf.Sin(time) - 3 * Mathf.Cos(time));
+
+        return position;
+    }
+
+    public static Vector3 CircleDiffEq(Vector3 position, int resolution, float time, float c1, float c2, int i) {
+        position.x = c1 * (-2 * Mathf.Cos(time)) + c2 * (-2 * Mathf.Sin(time));
+        position.y = c1 * (Mathf.Cos(time) + 3 * Mathf.Sin(time)) + c2 * (Mathf.Sin(time) - 3 * Mathf.Cos(time));
 
         return position;
     }
